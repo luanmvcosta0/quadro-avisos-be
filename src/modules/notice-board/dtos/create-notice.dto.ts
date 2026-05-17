40775d3f9c239d9a2c6642392709f7a0308cb7e3
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-17 14:00:40 -0300

    feat: add class validator

--- src/modules/notice-board/dtos/create-notice.dto.ts
+++ src/modules/notice-board/dtos/create-notice.dto.ts
@@ -1,5 +1,18 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateNoticeDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 30)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 15)
   category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 500)
   description: string;
 }
