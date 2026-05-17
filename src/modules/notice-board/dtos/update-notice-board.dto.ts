275416a9b2d5ac7cdb886e8498ca215b30382f40
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-17 10:10:56 -0300

    feat: add NoticeBoardEntity and update related DTOs and services

--- src/modules/notice-board/dtos/update-notice-board.dto.ts
+++ src/modules/notice-board/dtos/update-notice-board.dto.ts
@@ -1,1 +1,4 @@
-export class UpdateNoticeBoardDto {}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateNoticeBoardDto } from './create-notice-board.dto';
+
+export class UpdateNoticeBoardDto extends PartialType(CreateNoticeBoardDto) {}
