96dd5eb8b22f0f07a9c0e656ea37254b6343fb52
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-17 10:26:35 -0300

    fix: correct import path for NoticeBoardModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { NoticeBoardModule } from './modules/notice-board/notice-board.module';
+import { NoticeBoardModule } from './modules/notice-board/notice.module';
 
 @Module({
   imports: [NoticeBoardModule],
