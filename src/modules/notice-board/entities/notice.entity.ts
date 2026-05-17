47e509e33f96b351793be712f537af2df52ec44d
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-17 12:46:51 -0300

    feat: update Dockerfile and docker-compose for development setup; fix NoticeEntity column types

--- src/modules/notice-board/entities/notice.entity.ts
+++ src/modules/notice-board/entities/notice.entity.ts
@@ -2,18 +2,15 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'notice-board' })
 export class NoticeEntity {
-  @PrimaryGeneratedColumn({ type: 'number', name: 'id' })
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
   id: number;
 
-  @Column({ type: 'string', name: 'title' })
+  @Column({ name: 'title', nullable: false })
   title: string;
 
-  @Column({ type: 'string', name: 'category' })
+  @Column({ name: 'category', nullable: false })
   category: string;
 
-  @Column({ type: 'string', name: 'description' })
+  @Column({ name: 'description', nullable: false })
   description: string;
-
-  @Column({ type: 'string', name: 'user' })
-  user: string;
 }
