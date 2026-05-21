ddb074b73d076ef9d96748d3687ce395a0d08927
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-20 22:14:33 -0300

    feat(swagger): enhance notice creation with detailed DTO and controller documentation

--- src/modules/notice-board/entities/notice.entity.ts
+++ src/modules/notice-board/entities/notice.entity.ts
@@ -1,16 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'notice-board' })
 export class NoticeEntity {
+  @ApiProperty({ description: 'Id do aviso', example: '1' })
   @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
   id: number;
 
+  @ApiProperty({
+    description: 'Titulo do aviso',
+    example: 'Manutenção no Sistema',
+  })
   @Column({ name: 'title', nullable: false })
   title: string;
 
+  @ApiProperty({ description: 'Categoria do aviso', example: 'Comunicado' })
+  @ApiProperty({ description: 'id do aviso', example: '1' })
   @Column({ name: 'category', nullable: false })
   category: string;
 
+  @ApiProperty({
+    description: 'Descrição do aviso',
+    example:
+      'O sistema ficará indisponível neste sábado das 22h às 00h para atualização e melhorias de desempenho.',
+  })
+  @ApiProperty({ description: 'id do aviso', example: '1' })
   @Column({ name: 'description', nullable: false })
   description: string;
 }
--- src/modules/notice-board/notice.controller.ts
+++ src/modules/notice-board/notice.controller.ts
@@ -2,16 +2,36 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateNoticeDto } from './dtos/create-notice.dto';
 import { NoticeService } from './notice.service';
 import { NoticeEntity } from './entities/notice.entity';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('notice')
 export class NoticeController {
   constructor(private readonly noticeService: NoticeService) {}
 
+  @ApiOperation({
+    summary: 'Criar novo aviso',
+    description: 'Criação de novos avisos',
+  })
+  @ApiResponse({ status: 201, description: 'Aviso criado com sucesso' })
+  @ApiResponse({ status: 400, description: 'Pedido inválido' })
+  @ApiResponse({ status: 500, description: 'Erro interno no servidor' })
   @Post('/')
   createNotice(@Body() dto: CreateNoticeDto): Promise<NoticeEntity> {
     return this.noticeService.createNotice(dto);
   }
 
+  @ApiOperation({
+    summary: 'Buscar um aviso',
+    description: 'Busca de apenas um aviso',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Aviso retornado com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Aviso não encontrado',
+  })
   @Get('/')
   findNotice() {
     return this.noticeService.findNotice();
